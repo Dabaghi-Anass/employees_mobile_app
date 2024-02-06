c3eeba857fae006e62ef224dd685d3ff48ecdddf
Dabaghi-Anass <anass.dabaghi123@gmail.com>
2024-02-06 18:55:20 +0100

    completed Image picker

--- components/ImageSelector.jsx
+++ components/ImageSelector.jsx
@@ -1,47 +1,74 @@
 import { Ionicons } from '@expo/vector-icons';
+import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 import React, { useEffect, useState } from 'react';
 import { Image, StyleSheet, TouchableNativeFeedback, View } from 'react-native';
 import colors from '../constants/colors';
-import Error from './Error';
 
 const ImageSelector = ({ imageUrl, onImageTaken }) => {
     const [selectedImage, setSelectedImage] = useState(imageUrl);
-    const [error, setError] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+    const [camera, setCamera] = useState(null);
     
     async function askPermission() {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.MEDIA_LIBRARY);
-        return status === 'granted'
+        const cameraStatus = await Camera.requestCameraPermissionsAsync();
+        setHasCameraPermission(cameraStatus.status === 'granted');
+        const galleryStatus = await MediaLibrary.requestPermissionsAsync();
+        setHasGalleryPermission(galleryStatus.status === 'granted');
     }
     useEffect(() => {
-        askPermission().then((status) => { if(!status) setError('You need to grant permission to use this feature') });
-        if (error) {
-            setError(error);
-        }
+        askPermission()
     }, []);
     useEffect(()=>{
         setSelectedImage(imageUrl)
     }, [imageUrl])
-    const takeImageHandler = async () => {
-        const image = await ImagePicker.launchCameraAsync({
-            allowsEditing: true,
-            aspect: [1, 1],
-            quality: 0.5
-        });
-        let uri = image.assets[0].uri;
-        if (!image.canceled) {
+    function populatePickedImage(image) {
+        let uri = image?.assets?.[0]?.uri;
+        if (!image?.canceled) {
             setSelectedImage(uri);
             onImageTaken(uri);
         }
+    }
+    const takePicture = async () => {
+        if(!camera) return;
+        try{
+            const photo = await ImagePicker.launchCameraAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                aspect: [1,1],
+                quality: 1,
+            });
+            populatePickedImage(photo);
+        }catch(error){
+            console.log(error.message)
+        }
     };
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1,1],
+            quality: 1,
+        });
+        populatePickedImage(result);
+    };
+    
     return (
         <>
-            {error && <Error message={error} />}
+            <Camera
+                ref={(ref) => setCamera(ref)}
+                style={{ flex: 1 }}
+                ratio="4:3"
+            />
             <View style={styles.imagePicker}>
                 <Image style={styles.image} source={{ uri: selectedImage }} />
-                <TouchableNativeFeedback onPress={takeImageHandler}>
-                    <Ionicons style={styles.imageButton} name="camera" size={30} color={colors.TEXT_100} />
+                <TouchableNativeFeedback onPress={takePicture}>
+                    <Ionicons style={styles.imageButton} name="camera" size={40} color={colors.TEXT_100} />
+                </TouchableNativeFeedback>
+                <TouchableNativeFeedback onPress={pickImage}>
+                    <Ionicons style={{...styles.imageButton, ...styles.cameraButton}} name="image" size={40} color={colors.TEXT_100} />
                 </TouchableNativeFeedback>
             </View>
         </>
@@ -67,7 +94,15 @@ var styles = StyleSheet.create({
         borderRadius : 50,
         backgroundColor : colors.BG_300 ,
         padding : 10,
+        right : 0,
+        transform : [{translateX : -15 } , {translateY : 10}],
         borderColor: colors.BG_300
+    },
+    cameraButton:{
+        left : 0,
+        width : 60,
+        aspectRatio : 1,
+        transform : [{translateX : 15 } , {translateY : 10}],
     }
 })
 
